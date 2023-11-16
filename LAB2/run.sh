7054ca6dc6f72ef43fe59467d3a4d80c32ab2dd0
Aint <pinevichv@gmail.com>
2023-11-16 20:10:14 +0300

    changed sol

--- LAB2/run.sh
+++ LAB2/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name=LAB-2-test
-#SBATCH --time=00:15:00
-#SBATCH --nodes=1 --cpus-per-task=18
+#SBATCH --time=00:05:00
+#SBATCH --nodes=4 --cpus-per-task=1
 #SBATCH --partition release
 ulimit -l unlimited
-mpirun -n 1 /nethome/student/FS20/FS2-x1/Pinevich_Sukhova/LAB2/executable
+mpirun -n 4 /nethome/student/FS20/FS2-x1/Pinevich_Sukhova/LAB2/executable
