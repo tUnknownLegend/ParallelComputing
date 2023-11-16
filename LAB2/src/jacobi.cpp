c05ae0c5534954b338301d1d53b43541b818dd12
Aint <pinevichv@gmail.com>
2023-11-16 04:03:40 +0300

    refactoring

--- LAB2/src/jacobi.cpp
+++ LAB2/src/jacobi.cpp
@@ -16,7 +16,7 @@ void JacobiSendRecv(const int myId,
     int scount = (myId != (numOfProcessors - 1)) ? n : 0; // у myId == numOfProcessors - 1 = 0
     int rcount = myId ? n : 0; // myId == 0 = 0
 
-    double t1 = -MPI_Wtime();
+    double stopWatch = -MPI_Wtime();
 
     int iterationsNum = 0;
 
@@ -26,7 +26,8 @@ void JacobiSendRecv(const int myId,
     vector<double> temp(solution.size());
 
     double norm_local, norm_err;
-    while (norm_err < COMPARE_RATE) {
+    bool flag = true;
+    while (flag) {
         iterationsNum++;
         std::swap(temp, solution);
 
@@ -75,9 +76,10 @@ void JacobiSendRecv(const int myId,
 
         MPI_Allreduce(&norm_local, &norm_err, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 
+        if (norm_err < COMPARE_RATE)
+            flag = false;
     }
-
-    t1 += MPI_Wtime();
+    stopWatch += MPI_Wtime();
 
     for (int i = 0; i < numOfProcessors; ++i) {
         str_per_proc[i] *= n;
@@ -102,120 +104,18 @@ void JacobiSendRecv(const int myId,
         std::cout << "\n\n 1. Send + Recv";
         std::cout << "\n\t norm: " << proverka(y, analitical_sol);
         std::cout << "\n\t iterationsNum: " << iterationsNum;
-        printf("\n\t time: %.4f", t1);
-    }
-}
-
-void JacobiSendAndRecv(const int myId,
-                       const int numOfProcessors) {
-    vector<int> str_per_proc, nums_start;
-    int str_local, nums_local;
-    double norm_local, norm_err;
-
-    str_split(myId, numOfProcessors, str_local, nums_local, str_per_proc, nums_start);
-
-    double t1 = -MPI_Wtime();
-
-    int sourceProcess = myId ? myId - 1 : numOfProcessors - 1;
-    int destProcess = (myId != (numOfProcessors - 1)) ? myId + 1 : 0;
-
-    int scount = (myId != (numOfProcessors - 1)) ? n : 0;
-    int rcount = myId ? n : 0;
-
-    int iterationsNum = 0;
-
-    vector<double> solution(str_local * n);
-    vector<double> nextTopSolution(n);
-    vector<double> prevBottomSolution(n);
-    vector<double> temp(solution.size());
-    while (norm_err < COMPARE_RATE) {
-        iterationsNum++;
-
-        std::swap(temp, solution);
-
-        // пересылаем нижние и верхние строки
-        MPI_Sendrecv(temp.data() + (str_local - 1) * n, scount, MPI_DOUBLE, destProcess, 42, prevBottomSolution.data(),
-                     rcount,
-                     MPI_DOUBLE, sourceProcess, 42, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-        MPI_Sendrecv(temp.data(), rcount, MPI_DOUBLE, sourceProcess, 46, nextTopSolution.data(), scount, MPI_DOUBLE,
-                     destProcess,
-                     46, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-
-        /* пересчитываем все строки в полосе кроме верхней и нижней */
-        for (int i = 1; i < str_local - 1; ++i)
-            for (int j = 1; j < n - 1; ++j)
-                solution[i * n + j] =
-                        (temp[(i + 1) * n + j] + temp[(i - 1) * n + j] + temp[i * n + (j + 1)] + temp[i * n + (j - 1)] +
-                         pow(h, 2) * right_part((nums_local + i) * h, j * h)) * yMultiplayer;
-
-        /* пересчитываем верхние строки */
-        if (myId != 0)
-            for (int j = 1; j < n - 1; ++j)
-                solution[j] = (temp[n + j] + prevBottomSolution[j] + temp[j + 1] + temp[j - 1] +
-                               pow(h, 2) * right_part(nums_local * h, j * h)) * yMultiplayer;
-
-        /* пересчитываем нижние строки */
-        if (myId != numOfProcessors - 1)
-            for (int j = 1; j < n - 1; ++j)
-                solution[(str_local - 1) * n + j] =
-                        (nextTopSolution[j] + temp[(str_local - 2) * n + j] + temp[(str_local - 1) * n + (j + 1)] +
-                         temp[(str_local - 1) * n + (j - 1)] +
-                         pow(h, 2) * right_part((nums_local + (str_local - 1)) * h, j * h)) * yMultiplayer;
-
-        norm_local = proverka(temp, solution);
-
-        MPI_Allreduce(&norm_local, &norm_err, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-
-    }
-
-    t1 += MPI_Wtime();
-
-    for (int i = 0; i < numOfProcessors; ++i) {
-        str_per_proc[i] *= n;
-        nums_start[i] *= n;
-    }
-
-    vector<double> y;
-    if (myId == 0)
-        y.resize(n * n);
-
-    MPI_Gatherv(solution.data(), str_local * n, MPI_DOUBLE, y.data(), str_per_proc.data(), nums_start.data(),
-                MPI_DOUBLE,
-                0, MPI_COMM_WORLD);
-
-    if (myId == 0) {
-        // точное решение
-        vector<double> analitical_sol(n * n);
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                analitical_sol[i * n + j] = u_exact(i * h, j * h);
-
-        std::cout << "\n\n 2. SendRecv";
-        std::cout << "\n\t norm: " << proverka(y, analitical_sol);
-        std::cout << "\n\t iterationsNum: " << iterationsNum;
-        printf("\n\t time: %.4f", t1);
+        printf("\n\t time: %.4f", stopWatch);
     }
 }
 
 void JacobiISendIRecv(const int myId,
                       const int numOfProcessors) {
-    MPI_Request *send_req1;
-    MPI_Request *send_req2;
-    MPI_Request *recv_req1;
-    MPI_Request *recv_req2;
+
 
     vector<int> str_per_proc, nums_start;
     int str_local, nums_local;
-    double norm_local, norm_err;
 
     str_split(myId, numOfProcessors, str_local, nums_local, str_per_proc, nums_start);
-
-    send_req1 = new MPI_Request[2], recv_req1 = new MPI_Request[2];
-    send_req2 = new MPI_Request[2], recv_req2 = new MPI_Request[2];
-
-    vector<double> solution(str_local * n);
-    vector<double> nextTopSolution(n);
-    vector<double> prevBottomSolution(n);
 
     int sourceProcess = myId ? myId - 1 : numOfProcessors - 1; // myId == 0 = numOfProcessors - 1
     int destProcess = (myId != (numOfProcessors - 1)) ? myId + 1 : 0; // у myId == numOfProcessors - 1 = 0
@@ -223,7 +123,21 @@ void JacobiISendIRecv(const int myId,
     int scount = (myId != (numOfProcessors - 1)) ? n : 0; // у myId == numOfProcessors - 1 = 0
     int rcount = myId ? n : 0; // myId == 0 = 0
 
+    vector<double> solution(str_local * n);
+    vector<double> nextTopSolution(n);
+    vector<double> prevBottomSolution(n);
     vector<double> temp(solution.size());
+
+    double norm_local, norm_err;
+    int iterationsNum = 0;
+    bool flag = true;
+    
+    double stopWatch = -MPI_Wtime();
+    
+    auto *send_req1 = new MPI_Request[2];
+    auto *send_req2 = new MPI_Request[2];
+    auto *recv_req1 = new MPI_Request[2];
+    auto *recv_req2 = new MPI_Request[2];
 
     // пересылаем верхние и нижние строки temp
     MPI_Send_init(temp.data(), rcount, MPI_DOUBLE, sourceProcess, 0, MPI_COMM_WORLD, send_req1);
@@ -240,14 +154,7 @@ void JacobiISendIRecv(const int myId,
                   send_req2 + 1);
     MPI_Recv_init(nextTopSolution.data(), scount, MPI_DOUBLE, destProcess, 0, MPI_COMM_WORLD, recv_req2 + 1);
 
-    vector<double> y;
-    if (myId == 0)
-        y.resize(n * n);
-
-    double t1 = -MPI_Wtime();
-
-    int iterationsNum = 0;
-    while (norm_err < COMPARE_RATE) {
+    while (flag) {
         iterationsNum++;
 
         std::swap(temp, solution);
@@ -300,14 +207,25 @@ void JacobiISendIRecv(const int myId,
         norm_local = proverka(temp, solution);
 
         MPI_Allreduce(&norm_local, &norm_err, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    }
 
-    t1 += MPI_Wtime();
+        if (norm_err < COMPARE_RATE)
+            flag = false;
+    }
+    delete[] send_req1;
+    delete[] recv_req1;
+    delete[] send_req2;
+    delete[] recv_req2;
+    
+    stopWatch += MPI_Wtime();
 
     for (int i = 0; i < numOfProcessors; ++i) {
         str_per_proc[i] *= n;
         nums_start[i] *= n;
     }
+
+    vector<double> y;
+    if (myId == 0)
+        y.resize(n * n);
 
     MPI_Gatherv(solution.data(), str_local * n, MPI_DOUBLE, y.data(), str_per_proc.data(), nums_start.data(),
                 MPI_DOUBLE,
@@ -323,11 +241,6 @@ void JacobiISendIRecv(const int myId,
         std::cout << "\n\n 3. Isend + Irecv";
         std::cout << "\n\t norm: " << proverka(y, analitical_sol);
         std::cout << "\n\t iterationsNum: " << iterationsNum;
-        printf("\n\t time: %.4f", t1);
+        printf("\n\t time: %.4f", stopWatch);
     }
-
-    delete[] send_req1;
-    delete[] recv_req1;
-    delete[] send_req2;
-    delete[] recv_req2;
 }
