6a3869124045a04b60272f1425d4ae84132fd964
Aint <pinevichv@gmail.com>
2023-12-14 19:24:10 +0300

    lab4

--- LAB4/run.sh
+++ LAB4/run.sh
@@ -4,4 +4,4 @@
 #SBATCH --nodes=1 --ntasks-per-node=16
 #SBATCH --partition release
 ulimit -l unlimited
-mpirun -n 16 /nethome/student/FS20/FS2-x1/Pinevich_Sukhova/LAB4_test/run
+mpirun -n 16 /nethome/student/FS20/FS2-x1/Pinevich_Sukhova/LAB4/run
