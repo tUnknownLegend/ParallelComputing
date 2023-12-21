76a82ea9e2c6cf407b453d8beeed3d13654e693f
Aint <pinevichv@gmail.com>
2023-12-21 23:24:40 +0300

    new

--- LAB4/kernel.cu
+++ LAB4/kernel.cu
@@ -55,7 +55,7 @@ acceleration(TYPE *cudaWeight, TYPE *cudaPosition, TYPE *cudaVelocity, TYPE *dev
 
     TYPE diff0, diff1, diff2, norm, mul, a0 = 0.0, a1 = 0.0, a2 = 0.0;
 
-//    TYPE r0 = cudaPosition[globIdx3], r1 = cudaPosition[globIdx3 + 1], r2 = cudaPosition[globIdx3 + 2];
+    TYPE r0 = cudaPosition[globIdx3], r1 = cudaPosition[globIdx3 + 1], r2 = cudaPosition[globIdx3 + 2];
 
     for (int i = 0; i < N; i += blockSize) {
         sharedWeight[locIdx] = cudaWeight[i + locIdx];
@@ -68,9 +68,12 @@ acceleration(TYPE *cudaWeight, TYPE *cudaPosition, TYPE *cudaVelocity, TYPE *dev
 #pragma unroll
         for (int j = 0; j < N - i; ++j) {
             if (i + j < N) {
-                diff0 = cudaPosition[globIdx3] - sharedPosition[3 * j];
-                diff1 = cudaPosition[globIdx3 + 1] - sharedPosition[3 * j + 1];
-                diff2 = cudaPosition[globIdx3 + 2] - sharedPosition[3 * j + 2];
+//                diff0 = cudaPosition[globIdx3] - sharedPosition[3 * j];
+                diff0 = r0 - sharedPosition[3 * j];
+//                diff1 = cudaPosition[globIdx3 + 1] - sharedPosition[3 * j + 1];
+                diff1 = r1 - sharedPosition[3 * j + 1];
+//                diff2 = cudaPosition[globIdx3 + 2] - sharedPosition[3 * j + 2];
+                diff2 = r2 - sharedPosition[3 * j + 2];
 
                 norm = diff0 * diff0 + diff1 * diff1 + diff2 * diff2;
 
